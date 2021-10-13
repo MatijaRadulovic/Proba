ad872162e194072d586521b1c7e0b5b79e3256c5
MatijaRadulovic <89700052+MatijaRadulovic@users.noreply.github.com>
2021-10-13 19:12:23 +0200

    Dodavanje pocetnog dela
    Dodao sam pocetni deo bruh.

---
+++ Konzolna proba/PrvaKlasa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzolna_proba
+{
+    class PrvaKlasa
+    {
+        public static void ProbajMe() { Console.WriteLine("Probao si klasu 1"); }
+        
+    }
+}
